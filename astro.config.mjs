9c6b356599314e0a3c693975dc196a00ef2b8792
Kristan Ruiz Limon <itan_07@hotmail.com>
2026-03-17 10:34:33 -0700

    first: code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,42 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightThemeGalaxy from "starlight-theme-galaxy";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Whatsbotcord | Documentation",
+      // plugins: [starlightThemeGalaxy()],
+      social: [{ icon: "github", label: "GitHub", href: "https://github.com/withastro/starlight" }],
+      sidebar: [
+        {
+          label: "Getting started",
+          autogenerate: { directory: "gettingstarted" },
+        },
+        {
+          label: "Guides",
+          items: [
+            // Starlight will automatically look in /en/guides/ or /es/guides/ based on the user's language
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+      ],
+      defaultLocale: "root", // Tells Starlight the root folder is the default
+      locales: {
+        root: {
+          // Defines the root folder as English
+          label: "English",
+          lang: "en",
+        },
+        es: {
+          // Defines the /es/ folder as Spanish
+          label: "Español",
+          lang: "es",
+        },
+      },
+    }),
+  ],
 });
+
