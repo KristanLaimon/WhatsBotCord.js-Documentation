7b291df8966ea131a92022a5b23c1daa9150c053
Kristan Ruiz Limon <itan_07@hotmail.com>
2026-06-16 19:26:16 -0700

    fix: Making documentation chat by default in Playground

--- src/components/whatsbotcord/PlaygroundLayout.svelte
+++ src/components/whatsbotcord/PlaygroundLayout.svelte
@@ -11,7 +11,7 @@
   
   let isLoadingCode = $state(true);
   let codeToUse = $state("");
-  let activeChatIdToUse = $state(999);
+  let activeChatIdToUse = $state(998);
   let initialMessageInput = $state("");
 
 
